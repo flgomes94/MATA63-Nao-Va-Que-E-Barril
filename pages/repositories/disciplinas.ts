dbdd337497017b7804f29608c91d86d7e2bd1656
Fabio Lopes <fabiolopes.dev@gmail.com>
2022-07-03 14:46:27 -0300

    Adicionando disciplina pra caramba

--- pages/repositories/disciplinas.ts
+++ pages/repositories/disciplinas.ts
@@ -10,10 +10,42 @@ class DisciplinasRepository {
 
   constructor() {
     this.disciplinas = Array();
-    this.addDisciplina('Engenharia de Software 2', 'MATA63', 'Universidade Federal da Bahia - UFBA');
-    this.addDisciplina('Inteligência Artificial', 'MATA64', 'Universidade Federal da Bahia - UFBA');
-    this.addDisciplina('Sistemas Multimídias', 'MATB19', 'Universidade Federal da Bahia - UFBA');
-    this.addDisciplina('Introdução a Web Semântica', 'MATC93', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('GEOMETRIA ANALÍTICA', 'MATA01', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('CÁLCULO A', 'MATA02', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('INTRODUÇÃO À LÓGICA DE PROGRAMAÇÃO', 'MATA37', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('PROJETO DE CIRCUITOS LÓGICOS', 'MATA38', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('SEMINÁRIOS DE INTRODUÇÃO AO CURSO', 'MATA39', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('MATEMÁTICA DISCRETA I', 'MATA42', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('ÁLGEBRA LINEAR A', 'MATA07', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('ESTRUTURAS DE DADOS E ALGORITMOS I', 'MATA40', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('ARQUITETURA DE COMPUTADORES', 'MATA48', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('LABORATÓRIO DE PROGRAMAÇÃO I', 'MATA57', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('COMPLEMENTOS DE CÁLCULO', 'MATA95', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('MATEMÁTICA DISCRETA II', 'MATA97', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('ELEMENTOS DO ELETROMAGNETISMO E DE CIRCUITOS ELÉTRICOS', 'FISA75', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('MÉTODOS ESTATÍSTICOS', 'MAT236', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('LÓGICA PARA COMPUTAÇÃO', 'MATA47', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('PROGRAMAÇÃO DE SOFTWARE BÁSICO', 'MATA49', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('LINGUAGENS FORMAIS E AUTÔMATOS', 'MATA50', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('PROGRAMAÇÃO ORIENTADA A OBJETOS', 'MATA55', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('METODOLOGIA E EXPRESSÃO TÉCNICO-CIENTÍFICA', 'FCHC45', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('TEORIA DA COMPUTAÇÃO', 'MATA51', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('ANÁLISE E PROJETO DE ALGORITMOS', 'MATA52', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('SISTEMAS OPERACIONAIS', 'MATA58', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('COMPILADORES', 'MATA61', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('COMPUTADOR, ÉTICA E SOCIEDADE', 'MATA68', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('TEORIA DOS GRAFOS', 'MATA53', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('ESTRUTURAS DE DADOS E ALGORITMOS II', 'MATA54', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('REDES DE COMPUTADORES I', 'MATA59', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('ENGENHARIA DE SOFTWARE I', 'MATA62', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('PARADIGMAS DE LINGUAGENS DE PROGRAMAÇÃO A', 'MATE12', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('BANCO DE DADOS', 'MATA60', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('INTELIGÊNCIA ARTIFICIAL', 'MATA64', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('COMPUTAÇÃO GRÁFICA', 'MATA65', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('FUNDAMENTOS DE SISTEMAS DISTRIBUÍDOS', 'MATA88', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('ENGENHARIA DE SOFTWARE II-A', 'MATE11', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('PROJETO FINAL DE CURSO I', 'MATA66', 'Universidade Federal da Bahia - UFBA');
+    this.addDisciplina('PROJETO FINAL DE CURSO II', 'MATA67', 'Universidade Federal da Bahia - UFBA');
   }
 
   public static getInstance(): DisciplinasRepository {
